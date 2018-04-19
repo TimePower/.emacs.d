04e19df18773d764d21aa18b9e46a9456d9c4ff9
TimePower <timepower666@live.com>
2018-04-19 21:34:41 +0800

    MjAxOOW5tCAwNOaciCAxOeaXpSDmmJ/mnJ/lm5sgMjE6MzQ6MzYgQ1NUCg==

--- package/init-elpa.el
+++ package/init-elpa.el
@@ -2,11 +2,18 @@
 ;; (require 'use-package)
 (require 'package)
 ;; 增加软件包仓库
-(setq package-archives '(("elpa" . "https://elpa.gnu.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
-                         ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
-                         ("org" . "http://orgmode.org/elpa/")))
-;; 定义require-package函数
+
+(setq package-archives '(("elpa" . "https://elpa.emacs-china.org/gnu/")
+                         ("melpa" . "https://elpa.emacs-china.org/melpa/")
+                         ("melpa-stable" . "https://elpa.emacs-china.org/melpa-stable/")
+                         ("org" . "https://elpa.emacs-china.org/org/")
+                         ("emacswiki" . "https://elpa.emacs-china.org/emacswiki/")))
+;; (setq package-archives '(("elpa" . "https://elpa.gnu.org/packages/")
+;;                          ("melpa" . "http://melpa.milkbox.net/packages/")
+;;                          ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
+;;                          ("org" . "http://orgmode.org/elpa/")))
+
+;; 定义 require-package 函数
 ;; (defun require-package (package &optional min-version no-refresh)
 ;;   "Install given PACKAGE, optionally requiring MIN-VERSION.
 ;; If NO-REFRESH is non-nil, the available package lists will not be
@@ -18,8 +25,8 @@
 ;;       (progn
 ;;         (package-refresh-contents)
 ;;         (require-package package min-version t)))))
-;; 强行提前初始化ELPA。因为默认情况下Emacs在init.el加载完之后才开始初始化ELPA，
-;; 而我们把大多数包的初始化函数都放在init.el中，如果不提前初始化ELPA会导致后面的
+;; 强行提前初始化 ELPA。因为默认情况下 Emacs 在 init.el 加载完之后才开始初始化 ELPA，
+;; 而我们把大多数包的初始化函数都放在 init.el 中，如果不提前初始化 ELPA 会导致后面的
 ;; 初始化过程出错（对应的包文件还没有加载进来）。
 (package-initialize)
 (provide 'init-elpa)
